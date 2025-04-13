a2e3675df57d96cd6024f8cd893e1c0e6397bd1e
Cyryl Bojarski <cyryl.bojarski@microsoft.wsei.edu.pl>
2025-04-13 15:36:18 +0200

    GetLetterGrade override method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -15,5 +15,52 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
         }
 
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int count = 0;
+
+            if (Students.Count < 5) { 
+                throw new InvalidOperationException("Less than 5 students");
+            }
+
+            foreach(var student in Students)
+            {
+
+                if (student.AverageGrade > averageGrade) 
+                {
+                    count++; // liczba studentow ktorzy maja lepsza ocene
+                }
+
+            }
+
+
+            if (count < (0.2 * Students.Count))
+            {
+                return 'A';
+            }
+            if ((count >= (0.2 * Students.Count)) && (count < (0.4 * Students.Count)))
+            {
+                return 'B';
+            }
+            if ((count >= (0.4 * Students.Count)) && (count < (0.6 * Students.Count)))
+            {
+                return 'C';
+            }
+            if ((count >= (0.6 * Students.Count)) && (count < (0.8 * Students.Count)))
+            {
+                return 'D';
+            }
+            if (count >= (0.8 * Students.Count))
+            {
+                return 'F';
+            }
+            return 'N';
+
+          
+
+
+
+        }
+
     }
 }
